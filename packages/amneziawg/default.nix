babc80b826e481f50f1a473e46aec855d3ff9411
outworld66 <acidcarpet@gmail.com>
2026-08-09 19:23:54 +0300

    fix amnezia for 7.1.5

--- packages/amneziawg/default.nix
+++ packages/amneziawg/default.nix
@@ -1,16 +1,25 @@
-{ linuxPackages }:
+{
+  lib,
+  linuxPackages,
+}:
 
 let
   patchModule =
-    module:
-    module.overrideAttrs (oldAttrs: {
-      patches = (oldAttrs.patches or [ ]) ++ [ ./ipv6-api-linux-7.1.patch ];
+    kernelPackages:
+    kernelPackages.amneziawg.overrideAttrs (oldAttrs: {
+      patches =
+        (oldAttrs.patches or [ ])
+        ++ [ ./ipv6-api-linux-7.1.patch ]
+        # Linux 7.1.5 uses UDP tunnel helpers that take struct sock.
+        ++ lib.optionals (lib.versionAtLeast kernelPackages.kernel.version "7.1.5") [
+          ./udp-tunnel-api-linux-7.1.5.patch
+        ];
     });
 in
-(patchModule linuxPackages.amneziawg).overrideAttrs (oldAttrs: {
+(patchModule linuxPackages).overrideAttrs (oldAttrs: {
   passthru = (oldAttrs.passthru or { }) // {
     # Consumers with a non-default kernel must build the module against the
     # matching kernel package set.
-    withKernel = kernelPackages: patchModule kernelPackages.amneziawg;
+    withKernel = patchModule;
   };
 })
